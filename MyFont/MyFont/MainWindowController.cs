c4b87bf2de3f9e21fba8f05ac2293a219a7768e0
Vinay <vin.moorthy@gmail.com>
2015-09-29 12:46:49 +0530

    Added reference to the font

---
+++ MyFont/MyFont/MainWindowController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Foundation;
+using AppKit;
+
+namespace MyFont
+{
+	public partial class MainWindowController : NSWindowController
+	{
+		public MainWindowController (IntPtr handle) : base (handle)
+		{
+		}
+
+		[Export ("initWithCoder:")]
+		public MainWindowController (NSCoder coder) : base (coder)
+		{
+		}
+
+		public MainWindowController () : base ("MainWindow")
+		{
+		}
+
+		public override void AwakeFromNib ()
+		{
+			base.AwakeFromNib ();
+			lblWelcome.Font = NSFont.FromFontName ("SanFranciscoText-BoldItalic",12.0f);
+		}
+
+		public new MainWindow Window {
+			get { return (MainWindow)base.Window; }
+		}
+	}
+}
